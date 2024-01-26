a6333413a51e975bd8e485326d952f8e64640b56
ashok <ashokau23@gmail.com>
2024-01-26 21:01:21 +0530

    completed.

--- React/signup-form-react/src/App.js
+++ React/signup-form-react/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
       <h1>Signup Form in  React</h1>
       <fieldset>
         <form action="#" method="get">
+          <div className ="row g-3 align-items-center"></div>
               <label htmlFor="firstname">First Name</label>
               <input type="text"
               name="firstname"
@@ -22,17 +23,17 @@ function App() {
              placeholder="Enter your Last Name"
              required /> 
              <br /><br />  
-             <label htmlFor="email">E Mail Address</label>
+             <label htmlFor="email">E-Mail Address</label>
              <input type="email"
              name="email"
              id="email"
              placeholder="Enter your Email"
              requried /> 
              <br /><br />
-             <label htmlFor="email">EContact number</label>
-             <input type="email"
-             name="email"
-             id="lastname"
+             <label htmlFor="contact">Contact number</label>
+             <input type="text"
+             name="contact"
+             id="contact"
              placeholder="Enter your mobile Number"
              requried /> 
              <br /><br />
@@ -45,10 +46,71 @@ function App() {
              <input type="radio" name="gender" value="" id="other" />
              other
              <br /><br />
+             <label for="lang">Your Best Subject</label>
+             <br />
+             <input type="checkbox" name="lang" id="english" checked/>
+             English
+             <input type="checkbox" name="lang" id="maths" />
+             Maths
+             <input type="checkbox" name="lang" id="physics"/>
+             Physics
+             <br /><br />
+             <label htmlFor="file">Upload Resume</label>
+             <input 
+             type="file"
+             name="file"
+             id="file"
+             placeholder="Enter Upload File"
+             required 
+             />
+             <br /><br />
+             <label htmlFor="url">Enter URL</label>
+             <input 
+             type="url"
+             name="url"
+             id="url"
+             placeholder="Enter url"
+             required
+              />
+              <br /><br />
+              <label>Select Your Choice</label>
+              <select name="select" id="select">
+                <option value="" disabled selected>
+                  Select Your Ans
+                </option>
+                <optgroup label="Beginers">
+                  <option value="1">HTML</option>
+                  <option value="2">CSS</option>
+                  <option value="3">JavaScript</option>
+                </optgroup>
+                <optgroup label="Advance">
+                    <option value="1">React</option>
+                    <option value="2">Node</option>
+                    <option value="3">Express</option>
+                    <option value="4">MongoDB</option>
+                </optgroup>
+              </select>
+              <br /><br />
+              <label htmlFor="about">About</label>
+              <br />
+              <textarea 
+              name="about" 
+              id="about" 
+              cols="30" 
+              rows="10"></textarea>
+              <br /><br />
+              <label htmlFor="#">Submit OR Reset</label>
+              <br />
+              <button type="reset" value="reset">
+                Reset
+              </button>
+              <button type="submit" value="Submit">
+                Submit
+              </button>
         </form>
       </fieldset>
     </div>
-  )
+  );
 }
   
 
