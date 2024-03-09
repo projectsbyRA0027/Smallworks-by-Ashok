c95410a4142d31d986a3bdc75451f596105c960b
ashok <ashokau23@gmail.com>
2024-03-09 19:22:38 +0530

    common section completed.

---
+++ frontend-project/src/common/Heading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Heading = ({title,description}) => {
+  return (
+    <div>
+        <h1 className='font-bold text-4xl m-2 p-2 uppercase'>{title}</h1>
+        <p className='mb-8'>{description}</p>
+    </div>
+  );
+};
